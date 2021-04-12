8678a320d4a8bee622cd8a2f33dd34cc2c5a4d1c
mayumiasakawa <63479656+mayumiasakawa@users.noreply.github.com>
2021-04-12 21:24:29 +0900

    add to pagination

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,17 @@
     </v-app-bar>
     <v-content>
       <div class="text-center">
+        <v-list>
+          <v-list-item v-for="list in displayLists" :key="list.index">
+            <v-list-item-content>
+              <v-list-item-title>{{ list.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
         <v-pagination
         v-model="page"
         :length="6"
+        @input ="pageChange"
         >
         </v-pagination>
       </div>
@@ -23,7 +31,21 @@ export default {
   data () {
     return {
       page: 1,
+      lists: [],
+      displayLists: [],
+      pageSize: 10,
     }
+  },
+  methods: {
+  pageChange: function(pageNumber){
+    this.displayLists = this.lists.slice(this.pageSize*(pageNumber -1), this.pageSize*(pageNumber));
+  }, 
+},
+  mounted: function(){
+    this.lists= new Array(99).fill().map((v,i)=> {
+      return { id : i,title : 'Title' + i};
+    });
+    this.displayLists = this.lists.slice(0,this.pageSize);
   }
 };
 </script>
